a71b748f620a0af9f40504356bc69154a1460f27
Karl Wikström <karl@wikstrom.io>
2021-07-23 13:07:31 +0200

    Add config.fish

--- Makefile
+++ Makefile
@@ -4,8 +4,12 @@ current_dir := $(abspath $(dir mkfile_path))
 link = ln -sb ${current_dir}/$(1) $(2)/$(1)
 
 .PHONY: all
-all: .spacemacs
+all: .spacemacs config.fish
 
 .PHONY: .spacemacs
 .spacemacs:
 	$(call link,$@,~)
+
+.PHONY: config.fish
+config.fish:
+	$(call link,$@,~/.config/fish)
---
+++ config.fish
@@ -0,0 +1,16 @@
+set -U fish_greeting
+
+# SSH
+set -x SSH_AUTH_SOCK $XDG_RUNTIME_DIR/ssh-agent.socket
+
+# GPG
+set -x GPG_TTY (tty)
+
+# PATH
+fish_add_path $HOME/go/bin
+fish_add_path $HOME/.yarn/bin
+fish_add_path $HOME/.npm-global/bin
+fish_add_path $HOME/.local/bin
+
+set -x BROWSER /usr/bin/qutebrowser
+
