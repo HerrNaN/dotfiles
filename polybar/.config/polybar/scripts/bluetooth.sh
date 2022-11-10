312b7453fc8d480dc00110979c76927ff94e1a87
Karl Wikström <karl@wikstrom.io>
2022-11-10 13:29:36 +0100

    polybar: fontawesome and bluetooth touchup

--- polybar/.config/polybar/scripts/bluetooth.sh
+++ polybar/.config/polybar/scripts/bluetooth.sh
@@ -2,11 +2,11 @@
 
 if [ $(bluetoothctl show | grep "Powered: yes" | wc -c) -eq 0 ]
 then
-  echo "%{F#66ffffff}"
+  echo "%{F#66ffffff}"
 else
   if [ $(echo info | bluetoothctl | grep 'Device' | wc -c) -eq 0 ]
   then
-    echo ""
+    echo ""
   fi
-  echo "%{F#2193ff}"
+  echo "%{F#2193ff}"
 fi
