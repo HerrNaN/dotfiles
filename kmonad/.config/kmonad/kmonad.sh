7ea0a595f3262c0c0d5f323febcfe9debc6fbc92
Karl Wikström <karl@wikstrom.io>
2023-03-18 13:53:06 +0100

    kmonad: use only one kbd file

--- kmonad/.config/kmonad/kmonad.sh
+++ kmonad/.config/kmonad/kmonad.sh
@@ -1,12 +1,22 @@
 #!/bin/env sh
+#
+# This script enables starting kmonad for different devices using
+# only one .kbd file.
 
 case $1 in
-    ergo_860) KBD_DEV=`kbd_ergo_860 /dev/input/(grep -B 1 -A 9 "ERGO 860B Keyboard" /proc/bus/input/devices | grep -o -E 'event[0-9]+')` ;;
-    thinkpad) KBD_DEV="platform-i8042-serio-0-event-kbd" ;;
+    # The bluetooth keyboard doesn't use the same /dev device when it connects
+    # so we first need to find it.
+    ergo_860)
+        KBD_DEV=/dev/input/$(grep -B 1 -A 9 "ERGO 860B Keyboard" /proc/bus/input/devices | grep -o -E 'event[0-9]+') ;;
+
+    # The on board keyboard on my current laptop.
+    nembus)
+        KBD_DEV="/dev/input/by-path/platform-i8042-serio-0-event-kbd" ;;
+
     *) echo "invalid keyboard"; exit 1;;
 esac
 
 export KBD_DEV
-KBDCFG=$(envsubst < ergo_860.kbd)
+KBDCFG=$(envsubst < ~/.config/kmonad/config.kbd)
 
-kmonad -d <(echo "$KBDCFG")
+kmonad <(echo "$KBDCFG")
