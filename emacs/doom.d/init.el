fbb00233482ea29088c45d65a901d9367047b0b2
Karl Wikström <karl@wikstrom.io>
2024-08-05 21:45:29 +0200

    update

--- emacs/doom.d/init.el
+++ emacs/doom.d/init.el
@@ -179,7 +179,7 @@
         +jq)               ; ...with native support for reading and processing JSON responses
        ;;rst               ; ReST in peace
        ;;(ruby +rails)     ; 1.step {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
-       ;;(rust +lsp)         ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
+       (rust +lsp)         ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
        ;;scala             ; java, but good
        ;;(scheme +guile)   ; a fully conniving family of lisps
        (sh +fish)          ; she sells {ba,z,fi}sh shells on the C xor
