73000d0d7fc3f8f38b0fdc86f3e063a7feb92831
Karl Wikström <karl@wikstrom.io>
2024-08-06 09:45:03 +0200

    feat(emacs): add systemd-mode

--- emacs/doom.d/custom.el
+++ emacs/doom.d/custom.el
@@ -3,13 +3,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(gruvbox-theme systemd company-restclient restclient gradle-mode magit-delta cmake-mode tide)))
+ '(package-selected-packages '(systemd)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ts-fold-replacement-face ((t (:foreground nil :box nil :inherit font-lock-comment-face :weight light)))))
+ )
 ;(put 'downcase-region 'disabled nil)
 ;(put 'customize-group 'disabled nil)
